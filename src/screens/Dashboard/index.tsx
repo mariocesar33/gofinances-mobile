5595fc33a54bb4b187cff8bece3c0fda857d80c3
Mário César Silva <mcgato33@hotmail.com>
2021-07-12 17:27:27 -0100

    construindo o Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-
-import { Container, Title } from "./styles";
+import { 
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Logout,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: "https://avatars.githubusercontent.com/u/24758286?v=4"}}
+            />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Mário César</UserName>
+            </User>
+          </UserInfo>
+
+          <Logout></Logout>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
