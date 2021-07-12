d25b3a5b8fe0cffa1b85f34ea610bf5600368d55
Mário César Silva <mcgato33@hotmail.com>
2021-07-12 11:56:53 -0100

    Update styles.ts

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,9 +4,10 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: #999;
 `;
 
 export const Title = styled.Text`
-  
+  font-size: 24px;
+  font-weight: bold;
+  color: purple;
 `;
