452e4794c2527e65b1072d6589d9c5fb62b562f9
Mário César Silva <mcgato33@hotmail.com>
2021-07-12 12:43:21 -0100

    Update theme.ts

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors:{
+    primary: "#5636d3",
+
+    secondary: "#ff872c",
+    secondary_light: 'rgba(255, 135, 44, 0.3)',
+
+    success: "#12a454",
+    sucess_ligth: "rgba(18, 164, 84, 0.5)",
+
+    attention: "#e83f5b",
+    attention_ligth: "rgba(232, 63, 91, 0.5)",
+
+    shape: "#ffffff",
+    title: "#363f5f",
+    text: "#969cb2",
+    backdround: "#f0f2f5",
+  }
+}
